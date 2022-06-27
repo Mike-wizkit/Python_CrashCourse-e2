ff86b242eafe02333244b78e3ab369abe2d84b51
Mike-Wizkt <wizkit.it@gmail.com>
2022-06-27 20:55:27 +0200

    learning methods with . .title() .upper() .lower()

---
+++ chapter_2/strings/name.py
@@ -0,0 +1,9 @@
+# the .title after the name variable activates the title-module
+name = "ada lovelace"
+print(name.title())
+
+# on this rule it's the same .upper for all uppercase and .lower for all lowercase
+name = "Ada Lovelace"
+print(name.upper())
+print(name.lower())
+
