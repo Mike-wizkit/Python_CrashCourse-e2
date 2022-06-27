48067613b6bfc14a0a5a6312b245a08c511b4cd1
Mike-Wizkt <wizkit.it@gmail.com>
2022-06-27 20:52:48 +0200

    adding tabs and new lines chapter2

---
+++ chapter_2/whitespace_to_strings/whitespace.py
@@ -0,0 +1,15 @@
+# Normal print
+print("Python")
+
+# To add a tab to your text use the character combination: \t
+# like below
+print("\tPython")
+
+# To add a new line in a sting, use the character combination: \n
+# like below
+print("Languages:\nPython\nC\nJavaScript")
+
+# You can also combine tabs and newlines in a single string.
+# the string "\n\t" tells Python to move to a new line, and start
+# the next line with a tab (example below)
+print("Languages:\n\tPyhton\n\tC\n\t\JavaScript")
