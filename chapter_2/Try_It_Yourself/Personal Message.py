ca69b310f87269e3860ff9a581abe5c9906abbc1
Mike-Wizkt <wizkit.it@gmail.com>
2022-06-29 15:03:51 +0200

    edit, gave a description of what ill did in comments

--- chapter_2/Try_It_Yourself/Personal Message.py
+++ chapter_2/Try_It_Yourself/Personal Message.py
@@ -1,3 +1,5 @@
+# here i made hello a variable same with name and learn
+# then ill combined everything to print it as a string with f
 hello = "Hello"
 name = "Michael"
 learn = "would you like to learn some Python today?"
