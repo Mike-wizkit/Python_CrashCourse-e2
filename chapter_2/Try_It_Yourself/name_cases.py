b0fa6d1b0d9e21b1689de99be9825da469f5271d
Mike-Wizkt <wizkit.it@gmail.com>
2022-06-29 15:07:26 +0200

    here ill used made my name as a variable and used it for .title (used for represent a person) for .lower (used to keep everything lowercase) for .upper (used to keep everything uppercase)

---
+++ chapter_2/Try_It_Yourself/name_cases.py
@@ -0,0 +1,12 @@
+# here i use .title to represent my name
+name = "michael wizkit"
+print(name.title())
+
+# here i use .lower to keep everything lowercase
+name = "michael wizkit"
+print(name.lower())
+
+# here i use .upper to keep everything in uppercase
+name = "michael wizkit"
+print(name.upper())
+
