84f833f2cca1a46f37f971b6ea840267d18ee76f
Mike-Wizkt <wizkit.it@gmail.com>
2022-06-30 14:48:35 +0200

    here i made a sheet for integers

---
+++ chapter_2/numbers/integers.py
@@ -0,0 +1,21 @@
+# you can add (+), subtract (-), multiply (*), and devide (/) intergers in python
+print(2 + 3)
+
+print(3 - 2)
+
+print(2 * 3)
+
+print(3 / 2)
+
+# in a terminal session, Python simply returns the result of the operation
+# Pyton uses two multiplication symbols to represent exponents:
+print(3 ** 2)
+
+print(3 ** 3)
+
+print(10 ** 6)
+
+# Python supports the order of operations to,
+# so you can use multiple operations in one expression
+print(2 + 3*4)
+
