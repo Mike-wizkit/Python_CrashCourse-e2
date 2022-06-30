3ad6c357e79a5317096094de98e13c3a8a71dc15
Mike-Wizkt <wizkit.it@gmail.com>
2022-06-30 19:10:40 +0200

    constant is like a variable that stays the same trough the whole program to make a constant  you have to write  the variable in ALL CAPS that way it indictes that its a constant and  shouldn't be changed

---
+++ chapter_2/numbers/constants.py
@@ -0,0 +1,4 @@
+# an constant is like a variable that stays the same trough the whole program to make a constant
+# you have to write  the variable in ALL CAPS that way it indictes that its a constant and
+# shouldn't be changed
+MAX_CONNECTIONS = 5000
