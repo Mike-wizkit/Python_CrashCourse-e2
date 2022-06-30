c77ab3b4cb8d9278b4a72b4dcfda05c97c9ec5a9
Mike-Wizkt <wizkit.it@gmail.com>
2022-06-30 17:21:32 +0200

    when yoy divide any two numbers, even if they are integers that result in a whole number, you'll always get a float. with mixing between integers and floats it will result in a float as well.

---
+++ chapter_2/numbers/integers_and_floats.py
@@ -0,0 +1,10 @@
+# when yoy divide any two numbers, even if they are integers that result
+# in a whole number, you'll always get a float.
+# with mixing between integers and floats it will result in a float as well.
+print(4 / 2)
+
+print(1 + 2.0)
+
+print(2 * 3.0)
+
+print(3.0 ** 2)
