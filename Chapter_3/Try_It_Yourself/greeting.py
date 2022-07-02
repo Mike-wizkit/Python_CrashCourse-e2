d3cce76acdf4f3838fb5942f5b86dfadffc65dd8
Mike-Wizkt <wizkit.it@gmail.com>
2022-07-02 09:57:15 +0200

    readability update

--- Chapter_3/Try_It_Yourself/greeting.py
+++ Chapter_3/Try_It_Yourself/greeting.py
@@ -1,5 +1,5 @@
 dutch_greetings = ['Hallo', 'goedemorgen', 'goedendag', 'Goedenavond']
-print(f"When you want to greet a passenger you can say {dutch_greetings[0]}")
-print(f"When you meet someone in the morning you can greed them by saying {dutch_greetings[1].title()}")
-print(f"When you meet someone during the day you can greed them by saying {dutch_greetings[2].title()}")
-print(f"When you meet someone in the evening you can greed them by saying {dutch_greetings[3]}")
+print(f"When you want to greet a passenger you can say: {dutch_greetings[0]}")
+print(f"When you meet someone in the morning you can greed them by saying: {dutch_greetings[1].title()}")
+print(f"When you meet someone during the day you can greed them by saying: {dutch_greetings[2].title()}")
+print(f"When you meet someone in the evening you can greed them by saying: {dutch_greetings[3]}")
