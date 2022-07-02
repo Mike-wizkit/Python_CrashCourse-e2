3cf97eda1f47f1bcbc3699c9faa438b98c76ef0f
Mike-Wizkt <wizkit.it@gmail.com>
2022-07-02 09:55:27 +0200

    here ill made a list with the known names of my pet and printed 3 sentences with the variables chosen of the list and explain them with a print

---
+++ Chapter_3/Try_It_Yourself/names.py
@@ -0,0 +1,4 @@
+pet_names = ['Keaira', 'Smurf', 'Mugly']
+print(f"The names of my pet are {pet_names[0]}")
+print(f'When she was just a pup people new here as {pet_names[1]}')
+print(f"And she is also known as {pet_names[2]} which stands for Mega Ugly")
