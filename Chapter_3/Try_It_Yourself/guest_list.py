eaa99aeeda60abc2cea3d4cc84528b835713db6a
Mike-Wizkt <wizkit.it@gmail.com>
2022-07-02 22:54:09 +0200

    try it yourself assignment 3-4 made an invitation list and printed that in an invitation

---
+++ Chapter_3/Try_It_Yourself/guest_list.py
@@ -0,0 +1,3 @@
+invited = ['Albert Einstein', 'Nicola tesla', 'Johannes Gutenberg', 'Mike WizKit']
+print(f"Dear {invited[0]}\nI am organizing a dinner with {invited[1]} and {invited[2]},\nand we would love it if you"
+      f"could come as well.\n\nWith kind regards\n{invited[3]}")
