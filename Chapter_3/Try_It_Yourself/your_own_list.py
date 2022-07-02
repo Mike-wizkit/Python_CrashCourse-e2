cd05cf9d9cb0fc259b84f5c6c6da7c5fccc9f0f6
Mike-Wizkt <wizkit.it@gmail.com>
2022-07-02 10:11:01 +0200

    made a list with my favorites and explained the variables by printing

---
+++ Chapter_3/Try_It_Yourself/your_own_list.py
@@ -0,0 +1,5 @@
+my_favorites = ['Dogs', 'Outdoor', 'Camping', 'hobby', 'Code', 'Learning']
+print(f"My favorite animals are {my_favorites[0]}.")
+print(f"My favorite place to be is {my_favorites[1]}.")
+print(f"My favorite thing to do {my_favorites[1]}, is to take my {my_favorites[0]} and go {my_favorites[2]}.")
+print(f"My favorite {my_favorites[3]} is {my_favorites[5]} to {my_favorites[4]} as a professional.")
