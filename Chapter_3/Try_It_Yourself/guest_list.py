eca8a1de16d1eed0287864a495cb200d07895d53
Mike-Wizkt <wizkit.it@gmail.com>
2022-07-07 16:56:46 +0200

    try it yourself assignment 3-4 3-5 made an invitation list and printed that in an invitation and removed and added a new guest

--- Chapter_3/Try_It_Yourself/guest_list.py
+++ Chapter_3/Try_It_Yourself/guest_list.py
@@ -1,3 +1,10 @@
-invited = ['Albert Einstein', 'Nicola tesla', 'Johannes Gutenberg', 'Mike WizKit']
-print(f"Dear {invited[0]}\nI am organizing a dinner with {invited[1]} and {invited[2]},\nand we would love it if you"
-      f"could come as well.\n\nWith kind regards\n{invited[3]}")
+invited = ['Albert Einstein', 'Nicola tesla', 'Johannes Gutenberg']
+
+name = invited[0]
+print(f"{name}, please come to my dinner.")
+
+name = invited[1]
+print(f"{name}, please come to my dinner.")
+
+name = invited[2]
+print(f"{name}, please come to my dinner.")
